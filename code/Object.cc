985d7c55a81e72d3516934ed7c45054fcc49f2c6
Fredrik Björklund <frebj191@student.liu.se>
2016-12-09 12:36:37 +0100

    First try to make movement of sprites smoother at to fix error when enabling speed ups

--- code/Object.cc
+++ code/Object.cc
@@ -3,24 +3,91 @@
 
 void Object::move(sf::Vector2f direction, std::shared_ptr<Map> map)
 {
-    sprite.move(direction);
-    if (false) {map->print();}
-    if (!checkCollisions(direction, map))
+    if (direction.x != 0)
     {
-        sprite.move(-direction);
+        for (int i{1}; i <=abs(direction.x); i++)
+        {
+            sprite.move(direction.x/abs(direction.x), 0);
+
+            if (!checkCollisions(sf::Vector2f(direction.x, 0), map))
+            {
+                int dy = 50*round(sprite.getPosition().y/50.0)-sprite.getPosition().y;
+                if (abs(dy) <= 15 && dy != 0 && checkCollisions(sf::Vector2f(0, dy), map))
+                {
+                    sprite.move(0, dy/abs(dy));
+                    animate_sprite(sf::Vector2f(0, dy));
+                }
+                if (!checkCollisions(sf::Vector2f(direction.x, 0), map))
+                {
+                    sprite.move(-direction.x/abs(direction.x), 0);
+                    i = 100;
+                }
+            }
+            else
+            {
+                animate_sprite(direction);
+
+            }
+            updateMapIndex();
+        }
     }
-    else
+
+    else if (direction.y != 0)
     {
-        animate_sprite(direction);
+        for (int j{1}; j <=abs(direction.y); j++)
+        {
+            sprite.move(0, direction.y/abs(direction.y));
+
+            if (!checkCollisions(sf::Vector2f(0, direction.y), map))
+            {
+                int dx = 50*round(sprite.getPosition().x/50.0)-sprite.getPosition().x;
+                if (abs(dx) <= 15 && dx != 0 && checkCollisions(sf::Vector2f(dx, 0), map))
+                {
+                   sprite.move(dx/abs(dx), 0);
+                   animate_sprite(sf::Vector2f(dx, 0));
+                }
+                if (!checkCollisions(sf::Vector2f(0, direction.y), map))
+                {
+                    sprite.move(0, -direction.y/abs(direction.y));
+                    j = 100;
+                }
+            }
+            else
+            {
+                animate_sprite(direction);
+
+            }
+            updateMapIndex();
+
+        }
     }
-    updateMapIndex();
+
+
+
+    // sprite.move(direction);
+    // if (false) {map->print();}
+    // if (!checkCollisions(direction, map))
+    // {
+    //     sprite.move(-direction);
+    // }
+    // else
+    // {
+    //animate_sprite(direction);
+    // }
+    //updateMapIndex();
 }
 
 
 bool Object::checkCollisions(sf::Vector2f direction, std::shared_ptr<Map> map)
 {
-    if (direction.x != 0) {direction.x = direction.x/std::abs(direction.x);} //0, -1 or +1
-    if (direction.y != 0) {direction.y = direction.y/std::abs(direction.y);} //0, -1 or +1
+    if (direction.x != 0)
+    {
+        direction.x = direction.x/abs(direction.x);  //0, -1 or +1
+    }
+    if (direction.y != 0)
+    {
+        direction.y = direction.y/abs(direction.y); //0, -1 or +1
+    }
     //xIndexMap = round((sprite.getPosition().x)/TILE_WIDTH+direction.x);
     //yIndexMap = round((sprite.getPosition().y)/TILE_HEIGHT+direction.y);
     MapCoords mapIndex{static_cast<int>(round((sprite.getPosition().x)/TILE_WIDTH+direction.x)),
