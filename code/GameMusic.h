17c67309132c8d9a626bf04c204e79ccb5dda967
Henric Davidsson <henda274@student.liu.se>
2016-12-15 09:30:16 +0100

    Comments in GameMusic

--- code/GameMusic.h
+++ code/GameMusic.h
@@ -1,6 +1,16 @@
 /*
-Klassen GameMusic
+* FILNAMN:       GameMusic.h
+* PROJEKT:       TDDC76-Projekt
+* PROGRAMMERARE: Johan Almgren, johal611
+*                Fredrik Björklund, frebj191
+*                Henric Davidsson, henda274
+*                Nils Larsén, nilla000
+*
+* DATUM:         2016-12-15
+*
+* BESKRIVNING:   Inkluderingsfil för klassen GameMusic.
 */
+
 #ifndef GAMEMUSIC_H
 #define GAMEMUSIC_H
 
@@ -13,6 +23,7 @@ public:
     GameMusic();
     ~GameMusic() = default;
 
+    // Funktioner för att spela ljud
     void playMenuMusic();
     void playGameMusic();
     void explosionSound();
@@ -20,6 +31,7 @@ public:
     void deathSound();
 
 private:
+    // Datamedlemmar för att hålla de olika ljuden
     sf::Music menuMusic{};
     sf::Music gameMusic{};
     sf::Sound explosion{};
